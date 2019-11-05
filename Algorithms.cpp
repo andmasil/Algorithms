37eedfab6641b14010e52079e15819acdabae8c5
andmasil <andmasil@gmail.com>
2019-11-05 16:38:22 +0100

    call isConnected on user input

--- Algorithms.cpp
+++ Algorithms.cpp
@@ -11,11 +11,11 @@ using namespace std;
 int main()
 {
 	ifstream inFile("tinyUF.txt", ifstream::in);
+	int p = 0, q = 0, n = 0;
+	string line;
 
 	if (inFile.is_open())
 	{
-		int p = 0, q = 0, n = 0;
-		string line;
 		cout << "File opened" << endl;
 
 		getline(inFile, line);
@@ -32,5 +32,20 @@ int main()
 			quickUnionUF.connect(p, q);
 		}
 		inFile.close();
+
+		line.clear();
+		while (line != "q")
+		{
+			getline(cin, line);
+			try 
+			{
+				p = stoi(line.substr(0, line.find(" ")));
+				line.erase(0, line.find(" "));
+				q = stoi(line);
+				quickFindUF.isConnected(p, q);
+				quickUnionUF.isConnected(p, q);
+			}
+			catch (const std::exception & e) { cout << e.what() << endl; }
+		}
 	}
 }
