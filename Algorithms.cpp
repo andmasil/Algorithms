c361441ae1faf6c0082d9aceba60eb19e71f47d8
andmasil <andmasil@gmail.com>
2019-11-05 11:48:43 +0100

    read data from file

--- Algorithms.cpp
+++ Algorithms.cpp
@@ -1,9 +1,29 @@
 #include <iostream>
-
+#include <fstream>
+#include <iostream>
+#include <string>
 
 using namespace std;
 
 int main()
 {
-	cout << "hello world" << endl;
+	ifstream inFile("tinyUF.txt", ifstream::in);
+
+	if (inFile.is_open())
+	{
+		int p = 0, q = 0, n = 0;
+		string line;
+		cout << "File opened" << endl;
+
+		getline(inFile, line);
+		n = stoi(line);
+
+		while (getline(inFile, line))
+		{
+			p = stoi(line.substr(0, line.find(" ")));
+			line.erase(0, line.find(" "));
+			q = stoi(line);
+		}
+		inFile.close();
+	}
 }
